1038df229c5301f4b4aee05e714a62410e136ed9
jakejasminjk <jakejasminjk@gmail.com>
2021-02-18 22:01:57 +0000

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    env: {
+        MONGO_URI: "mongodb+srv://kervens:1234@todos.3zrdo.mongodb.net/platts?retryWrites=true&w=majority?"
+    }
+}
---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import fetch from 'isomorphic-unfetch';
+
+ const Home = ({users}) => {
+  users.map(user => console.log(user))
+  return (
+    <div>
+      {users.map(user => {
+        return(
+        <h1 key={user._id}>{user.name}</h1>
+        )
+      })}
+    </div>
+  )
+}
+
+Home.getInitialProps = async () => {
+  const res = await fetch('http://localhost:3000/api/users');
+  const { data } = await res.json();
+
+  return { users: data }
+}
+
+export default Home;
